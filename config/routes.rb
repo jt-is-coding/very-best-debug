2d1245078e5c3e5ffc2abea6cbf0094684b0db10
jeremybaxter <jrbaxter94@gmail.com>
2024-06-18 12:15:59 -0500

    Done:)

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index" })
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
-  post("/insert_user_record", { :controller => "venues", :action => "create" })
-  post("/update_users/:user_id", { :controller => "users", :action => "update" })
+  post("/insert_user_record", { :controller => "users", :action => "create" })
+  post("/update_user/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
